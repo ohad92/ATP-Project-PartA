7f55dafe3abb75441d8e17de3ab8da508494fb7f
ohad92 <73739848+ohad92@users.noreply.github.com>
2021-03-21 20:04:04 +0200

    2 methods mazegenerator2

--- src/algorithms/mazeGenerators/AMazeGenerator.java
+++ src/algorithms/mazeGenerators/AMazeGenerator.java
@@ -1,4 +1,33 @@
 package algorithms.mazeGenerators;
 
-public abstract class AMazeGenerator {
+public abstract class AMazeGenerator implements IMazeGenerator {
+    private int rows;
+    private int cols;
+
+    public AMazeGenerator(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void setCols(int cols) {
+        this.cols = cols;
+    }
+
+    @Override
+    public long measureAlgorithmTimeMillis(int row, int col) {
+
+        return 0;
+    }
 }
