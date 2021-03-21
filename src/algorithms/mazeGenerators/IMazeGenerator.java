8f17173f87741c8704a7e8401ff73e4d71d41fd8
ohad92 <73739848+ohad92@users.noreply.github.com>
2021-03-21 19:53:36 +0200

    2 methods mazegenerator1

--- src/algorithms/mazeGenerators/IMazeGenerator.java
+++ src/algorithms/mazeGenerators/IMazeGenerator.java
@@ -1,6 +1,7 @@
 package algorithms.mazeGenerators;
 
 public interface IMazeGenerator {
-    public Maze generatore(int row, int col);
-    public long measureAlgorithmTimeMillis(int row, int col);
+    Maze generatore(int row, int col);
+    long measureAlgorithmTimeMillis(int row, int col);
+
 }
