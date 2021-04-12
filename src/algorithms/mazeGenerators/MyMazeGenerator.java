8c457c206b34ddd0249af57e332aaf2dd34529b5
ohad92 <73739848+ohad92@users.noreply.github.com>
2021-04-12 15:14:22 +0300

    documentation to MyMazeGenerator

--- src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -11,36 +11,47 @@ public class MyMazeGenerator extends AMazeGenerator {
 
         Maze maze = new Maze(row, col);
 
+        // special case that maze is 2X2
         if(row == 2 && col == 2){
             IMazeGenerator mazeGenerator = new SimpleMazeGenerator();
             maze = mazeGenerator.generate(row,col);
             return maze;
         }
         maze.setRandomPositoins();
+        //Start with a grid full of walls.
         maze.setuniformmaze(1);
 
+        //Pick a cell, mark it as part of the maze
         maze.setcell(maze.getStartPosition(), 0);
         maze.setcell(maze.getGoalPosition(),0);
 
+        //Add the walls of the cell to the wall list.
         ArrayList<Position> frontiers = getNeighbors(maze.getStartPosition(),maze);
         ArrayList<Position> unVisited = new ArrayList<>();
 
+        //While there are walls in the list:
         while (!frontiers.isEmpty()) {
+            //Pick a random wall from the list
             Position randomPosition = randomPosition(frontiers);
+
             unVisited.clear();
             unVisited = maze.frontiers(randomPosition,0);
             Position A,B;
+            //If only one of the two cells that the wall divides is visited
             if (unVisited.size() == 1) {
                 A = new Position(randomPosition.getRowIndex(), randomPosition.getColumnIndex());
+                //Make the wall a passage
                 maze.setcell(A, 0);
 
                 A = unVisited.get(0);
                 if ((A.getRowIndex() >= 0) && (A.getRowIndex() < row) && (A.getColumnIndex() >= 0) && (A.getColumnIndex() < col)) {
                     B = new Position(A.getRowIndex(), A.getColumnIndex());
                     maze.setcell(B, 0);
+                    //Add the neighboring walls of the cell to the wall list.
                     frontiers.addAll(getNeighbors(A,maze));
                 }
             }
+            //Remove the wall from the list.
             frontiers.remove(randomPosition);
         }
 
